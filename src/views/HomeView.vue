ab775460fcc7216be604f28bcff9b0a5126fda6b
CristianoSantan <cristiano.mer@gmail.com>
2024-11-27 18:53:05 -0300

    aula 14 - customizando menu com vuetify

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,14 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Seja bem vindo!" title="Campus Code"/>
-  </div>
+
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
-export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
+  export default {
+    name: 'Home',
+
+    components: {
+      
+    },
   }
-}
 </script>
