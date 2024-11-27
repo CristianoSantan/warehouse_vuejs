c792789fb45c54abd1e19b3c5f82569c15779646
CristianoSantan <cristiano.mer@gmail.com>
2024-11-27 10:17:10 -0300

    aula 09 - exibindo os galpões cadastrados

--- src/views/WareHouseList.vue
+++ src/views/WareHouseList.vue
@@ -1,12 +1,32 @@
 <template>
   <div>
     <h1>Galpões Cadastrados</h1>
+
+    <table border="1px">
+      <thead>
+        <th>Códio</th>
+        <th>Nome</th>
+        <th>Sigla</th>
+      </thead>
+      <tbody>
+        <tr v-for="w in warehouses" :key="w.code">
+          <td>{{ w.id }}</td>
+          <td>{{ w.name }}</td>
+          <td>{{ w.code }}</td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
 <script>
   export default {
     name: 'WareHouseList',
+    data(){
+      return{
+        warehouses: []
+      }
+    },
 
     async mounted() {
       this.getWareHouses();
@@ -17,6 +37,9 @@
         const response = await fetch('http://localhost:3000/api/v1/warehouses', { method: 'GET' });
         const result = await response.json();
         console.log(result);
+
+        this.warehouses = result;
+        return this.warehouses;
       }
     }
   }
